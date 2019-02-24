a05e3d2783afc8c78a8c229472acca767c71558f
saber2pr <saber2pr@gmail.com>
2019-02-24 20:59:42 +0800

    update

--- src/core/saber-vdom.ts
+++ src/core/saber-vdom.ts
@@ -5,8 +5,21 @@
  * @Last Modified time: 2019-02-24 20:43:06
  */
 import * as CSS from 'csstype'
-
+/**
+ * VDom
+ *
+ * @export
+ * @class VDom
+ * @template T
+ */
 export class VDom<T extends keyof HTMLElementTagNameMap> {
+  /**
+   *Creates an instance of VDom.
+   * @param {T} name
+   * @param {string} inner
+   * @param {CSS.Properties} style
+   * @memberof VDom
+   */
   constructor(
     public name: T,
     public inner: string,
@@ -15,12 +28,51 @@ export class VDom<T extends keyof HTMLElementTagNameMap> {
     this.children = []
     this.props = {}
   }
+  /**
+   * Render
+   *
+   * @static
+   * @param {VDom<any>} vdom
+   * @param {HTMLElement} container
+   * @returns
+   * @memberof VDom
+   */
+  static Render(vdom: VDom<any>, container: HTMLElement) {
+    container.append(vdom.render())
+    return this
+  }
+  /**
+   * children
+   *
+   * @type {VDom<T>[]}
+   * @memberof VDom
+   */
   public children: VDom<T>[]
+  /**
+   * props
+   *
+   * @private
+   * @type {Partial<HTMLElementTagNameMap[T]>}
+   * @memberof VDom
+   */
   private props: Partial<HTMLElementTagNameMap[T]>
+  /**
+   * append
+   *
+   * @param {...VDom<any>[]} child
+   * @returns
+   * @memberof VDom
+   */
   append(...child: VDom<any>[]) {
     this.children.push(...child)
     return this
   }
+  /**
+   * render
+   *
+   * @returns
+   * @memberof VDom
+   */
   render() {
     const dom = document.createElement(this.name)
     dom.innerHTML = this.inner
@@ -33,10 +85,13 @@ export class VDom<T extends keyof HTMLElementTagNameMap> {
     this.children.forEach(vdom => dom.append(vdom.render()))
     return dom
   }
-  static Render(vdom: VDom<any>, container: HTMLElement) {
-    container.append(vdom.render())
-    return this
-  }
+  /**
+   * attr
+   *
+   * @param {Partial<HTMLElementTagNameMap[T]>} props
+   * @returns
+   * @memberof VDom
+   */
   attr(props: Partial<HTMLElementTagNameMap[T]>) {
     this.props = props
     return this
